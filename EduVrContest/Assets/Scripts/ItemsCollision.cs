8d80d612a351de03ea8dbbbc3b891607438007ad
Fabian <fabian_frontczak@wp.pl>
2019-12-03 00:35:20 +0100

    Podstawa do pierwszej minigry (kształty)
    1. Pobrano asset z gemami
    2. Zbudowano kostkę z kształtami do rozpoznania
    3. Rozpoczęto pracę nad sprawdzaniem kolizji

---
+++ EduVrContest/Assets/Scripts/ItemsCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsCollision : MonoBehaviour
+{
+    private Vector3 startingPos;
+    private Vector3 startingRot;
+
+    private void Awake()
+    {
+        startingPos = transform.position;
+        startingRot = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Items")
+        {
+            collision.collider.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        } 
+        else if (collision.collider.tag == "Ground")
+            {
+                transform.position = startingPos;
+                transform.rotation = new Quaternion(startingRot.x, startingRot.y, startingRot.z, 1.0f);
+            }
+    }
+}
