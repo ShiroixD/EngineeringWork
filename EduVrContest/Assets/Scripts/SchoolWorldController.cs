99b93a0b920d7927e8a34508880aec3be911a138
Shiroi <fabian_frontczak@wp.pl>
2020-01-05 19:26:43 +0100

    Dodano paczkę assetów szkolnych oraz utworzono scenę dla trzeciej gry

---
+++ EduVrContest/Assets/Scripts/SchoolWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchoolWorldController : MonoBehaviour, ISceneController
+{
+    public PlayerHelper PlayerHelper;
+    private System.Random rnd = new System.Random();
+
+    void Start()
+    {
+        InitializeScene();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void InitializeScene()
+    {
+
+    }
+
+    public void FinishScene()
+    {
+        PlayerHelper.ReturnToControlRoom();
+    }
+}
