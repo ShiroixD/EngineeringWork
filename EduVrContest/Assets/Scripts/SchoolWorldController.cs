03a9d4dff832dc0c19ff483a6d9807f114909701
Shiroi <fabian_frontczak@wp.pl>
2020-01-06 13:27:04 +0100

    Dodano losowanie zadania

--- EduVrContest/Assets/Scripts/SchoolWorldController.cs
+++ EduVrContest/Assets/Scripts/SchoolWorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SchoolWorldController : MonoBehaviour, ISceneController
 {
@@ -9,13 +10,17 @@ public class SchoolWorldController : MonoBehaviour, ISceneController
     public Sprite[] IconsSprites;
     public GameObject[] Bars;
     public GameObject[] Buttons;
-    private uint TASKS_LIMIT = 10;
-    private uint ANSWERS_AMOUNT = 3;
+    public SpriteRenderer TaskImage;
+    private int TASKS_LIMIT = 10;
+    private int ANSWERS_AMOUNT = 3;
     private System.Random _rnd = new System.Random();
-    private uint _score;
-    private uint _currentTaskId;
-    private uint _currentCorrectAnswear;
+    private int _score;
+    private int _currentTaskId;
+    private int _currentCorrectAnswear;
     private List<int> _usedTasksIds;
+    private const int BUTTON_DEFAULT_COLOR = 0xd0d0d0;
+    private const int BUTTON_GREEN_COLOR = 0x6ef864;
+    private const int BUTTON_RED_COLOR = 0xff675d;
 
     void Start()
     {
@@ -42,10 +47,70 @@ public class SchoolWorldController : MonoBehaviour, ISceneController
         PlayerHelper.ReturnToControlRoom();
     }
 
+    public Color hexToRgb(int number)
+    {
+        var r = (number >> 16) & 255;
+        var g = (number >> 8) & 255;
+        var b = number & 255;
+
+        return new Color(r, g, b);
+    }
+
     public void GiveTask()
     {
-        //wylosowac number id zadania
-        //wylosowac pozostale odpowiedzi
-        //wypelnic tesktu buttonow
+        if (_usedTasksIds.Count >= IconsNames.Length)
+        {
+            _usedTasksIds.Clear();
+        }
+        int number;
+        do
+        {
+            number = _rnd.Next(0, IconsNames.Length);
+        } while (_usedTasksIds.Contains(number));
+        _currentTaskId = number;
+        _currentCorrectAnswear = _rnd.Next(0, ANSWERS_AMOUNT);
+        string[] availableAnswers = new string[ANSWERS_AMOUNT];
+        availableAnswers[_currentCorrectAnswear] = IconsNames[_currentTaskId];
+        ArrayList otherAnswers = new ArrayList();
+        do
+        {
+            number = _rnd.Next(0, IconsNames.Length);
+            if (!otherAnswers.Contains(number) && number != _currentTaskId)
+            {
+                otherAnswers.Add(number);
+            }
+        } while (otherAnswers.Count < (ANSWERS_AMOUNT - 1));
+        number = 0;
+        for (int i = 0; i < availableAnswers.Length; i++)
+        {
+            if (availableAnswers[i] == null)
+            {
+                availableAnswers[i] = IconsNames[(int)otherAnswers[number]];
+                number++;
+            }
+        }
+        TaskImage.sprite = IconsSprites[_currentTaskId];
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            Buttons[i].transform.Find("Answer").gameObject.
+                GetComponent<TextMesh>().text = availableAnswers[i];
+        }
+    }
+
+    public void GiveAnswer(int number)
+    {
+        GameObject button = Buttons[number];
+        if (number == _currentCorrectAnswear)
+        {
+            Bars[_score].SetActive(true);
+            _score++;
+            button.transform.Find("BackCube").
+                gameObject.transform.Find("FrontCube").
+                gameObject.GetComponent<Renderer>().material.color = hexToRgb(BUTTON_GREEN_COLOR);
+        }
+        else
+        {
+
+        }
     }
 }
