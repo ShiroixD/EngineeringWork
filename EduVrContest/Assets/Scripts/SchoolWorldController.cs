b727c5c78048d9b50fd4f3396ef9144431cc7844
Shiroi <fabian_frontczak@wp.pl>
2020-01-06 01:04:03 +0100

    Ułożono scenę pod zadania gry oraz zaczęto wypełniać kontroler świata

--- EduVrContest/Assets/Scripts/SchoolWorldController.cs
+++ EduVrContest/Assets/Scripts/SchoolWorldController.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class SchoolWorldController : MonoBehaviour, ISceneController
 {
     public PlayerHelper PlayerHelper;
-    private System.Random rnd = new System.Random();
+    public string[] IconsNames;
+    public Sprite[] IconsSprites;
+    public GameObject[] Bars;
+    public GameObject[] Buttons;
+    private uint TASKS_LIMIT = 10;
+    private uint ANSWERS_AMOUNT = 3;
+    private System.Random _rnd = new System.Random();
+    private uint _score;
+    private uint _currentTaskId;
+    private uint _currentCorrectAnswear;
+    private List<int> _usedTasksIds;
 
     void Start()
     {
@@ -14,16 +24,28 @@ public class SchoolWorldController : MonoBehaviour, ISceneController
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            GiveTask();
+        }
     }
 
     public void InitializeScene()
     {
-
+        _score = 0;
+        _usedTasksIds = new List<int>();
+        GiveTask();
     }
 
     public void FinishScene()
     {
         PlayerHelper.ReturnToControlRoom();
+    }
+
+    public void GiveTask()
+    {
+        //wylosowac number id zadania
+        //wylosowac pozostale odpowiedzi
+        //wypelnic tesktu buttonow
     }
 }
