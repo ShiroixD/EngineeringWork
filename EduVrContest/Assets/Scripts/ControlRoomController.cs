7f6a4398938d7809953ab6ee4554af21288f5909
Shiroi <fabian_frontczak@wp.pl>
2020-01-08 11:58:24 +0100

    Dodano podpowiedź dla ControlRoom

--- EduVrContest/Assets/Scripts/ControlRoomController.cs
+++ EduVrContest/Assets/Scripts/ControlRoomController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ControlRoomController : MonoBehaviour, ISceneController
 {
     public GameObject[] Stars;
+    public GameObject GameInfo;
+    public GameObject ObjectToHideWhenInfo;
     private GameManager _gameManager;
+    private bool _showingInfo;
 
     void Start()
     {
@@ -44,11 +47,20 @@ public class ControlRoomController : MonoBehaviour, ISceneController
 
     public void ShowGameInfo()
     {
-        
+        if (!_showingInfo)
+        {
+            _showingInfo = true;
+            Vector3 pos = ObjectToHideWhenInfo.transform.position;
+            ObjectToHideWhenInfo.transform.position = new Vector3(pos.x, pos.y - 10.0f, pos.z);
+            GameInfo.SetActive(true);
+        }
     }
 
     public void HideGameInfo()
     {
-        
+        _showingInfo = false;
+        Vector3 pos = ObjectToHideWhenInfo.transform.position;
+        ObjectToHideWhenInfo.transform.position = new Vector3(pos.x, pos.y + 10.0f, pos.z);
+        GameInfo.SetActive(false);
     }
 }
